0b02a6b782ec7f7cd64c0caadc4b36f5184b48e3
ingenieriaensistemasvillalon-max <ingenieriaensistemasvillalon@gmail.com>
2025-12-03 08:58:31 -0600

    Fix variable names and comments in lista.py

--- lista.py
+++ lista.py
@@ -1,14 +1,14 @@
-#Ingrese una serie de numeros
-values = "Ingresar numeros"
+# Solicitar al usuario que ingrese una secuencia de números separados por comas y almacenarla en la variable 'valores'
+valores = input("Ingrese algunos números separados por comas: ")
 
-# Divide la cadena 'valores' en una lista usando comas como separadores y almacénala en la variable 'lista'
+# Divide la cadena 'valores' en una lista usando comas como separadores y almacénala en la variable 'list'
 list = values.split(",")
 
-# Convierte la 'lista' en una tupla y guárdala en la variable 'tupla'
-tupla = tupla(lista)
+# Convierte la 'lista' en una tuple y guárdala en la variable 'tuple'
+tuple = tuple(lista)
 
 # Imprimir la lista
 print('Lista: ', lista)
 
-# Imprimir la tupla
-print('Tupla: ', tupla)
+# Imprimir la tuple
+print('Tuple: ', tuple)
