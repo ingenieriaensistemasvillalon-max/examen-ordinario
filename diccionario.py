3f0ed60ff873f80b3709b482a20274400cac395c
ingenieriaensistemasvillalon-max <ingenieriaensistemasvillalon@gmail.com>
2025-12-03 08:42:34 -0600

    Create diccionario.py

---
+++ diccionario.py
@@ -0,0 +1,16 @@
+# Crear diccionario de productos y existencias
+inventario = {}
+
+# Capturar datos
+mouse["mouse"] = input("mouse":10)
+teclado["teclado"] = input("teclado":6)
+monitor["monitor"] = input("monitor":3)
+
+# Determinar si esta mouse,teclado,monitor
+existente = input("¿Existe? (True/False): ")
+
+# Mostrar resultados
+print(f"\nmouse: {mouse['10']} 
+print(f"teclado: {mouse['6']}")
+print(f"monitor: {monitor['3']}")
+print(f"existente: {existe}")
