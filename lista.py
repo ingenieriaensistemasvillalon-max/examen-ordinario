93dda698bfe922a8a426b48dcb1966908edba997
ingenieriaensistemasvillalon-max <ingenieriaensistemasvillalon@gmail.com>
2025-12-03 08:23:46 -0600

    Add functionality to input numbers and convert to tuple

---
+++ lista.py
@@ -0,0 +1,14 @@
+#Ingrese una serie de numeros
+values = "Ingresar numeros"
+
+# Divide la cadena 'valores' en una lista usando comas como separadores y almacénala en la variable 'lista'
+list = values.split(",")
+
+# Convierte la 'lista' en una tupla y guárdala en la variable 'tupla'
+tupla = tupla(lista)
+
+# Imprimir la lista
+print('Lista: ', lista)
+
+# Imprimir la tupla
+print('Tupla: ', tupla)
